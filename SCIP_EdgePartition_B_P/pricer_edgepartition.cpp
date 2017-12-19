66fe41fa0fe4d2be4fdf10b2b3ee31c4387635f3
william-jn-zhang <william1994dx@sina.com>
2017-12-19 15:09:41 +0800

    build pricer framework

--- SCIP_EdgePartition_B_P/pricer_edgepartition.cpp
+++ SCIP_EdgePartition_B_P/pricer_edgepartition.cpp
@@ -10,6 +10,8 @@
 
 #include "pricer_edgepartition.h"
 #include "cons_branchinfo.h"
+#include "scip/cons_setppc.h"
+#include "scip/cons_knapsack.h"
 
 #define PRICER_NAME            "pricer_edgeparititon"
 #define PRICER_DESC            "pricer for edge partition"
@@ -18,6 +20,7 @@
 
 struct SCIP_PricerData
 {
+	/* control infomation */
 	SCIP*                                scip;                   // scip data structure
 	int                                  maxvarsround;           // the maximum number of variable created in each round
 	int                                  oldmaxvarsround;        // 
@@ -31,7 +34,57 @@ struct SCIP_PricerData
 	int                                  nsetsfound;             // number of improving set found in the current round
 
 	SCIP_Bool                            usePriceHeur; //user set// using the heuristic to solve the price problem
+
+	/* data */
+	SCIP_Real*                           pi;                     // dual variable value
+	int                                  constraintssize;        // number of constraints
+	SCIP_CONS**                          constraints;            // a reference to the probdata -> constraints
+
 };
+
+static
+SCIP_DECL_PRICERINITSOL(pricerInitSolEdgepartition)
+{
+	SCIP_PRICERDATA* pricerdata;
+	SCIP_PROBDATA*   probdata;
+
+	assert(scip != NULL);
+	assert(pricer != NULL);
+
+	pricerdata = SCIPpricerGetData(pricer);
+	assert(pricerdata != NULL);
+
+	probdata = SCIPgetProbData(scip);
+	assert(probdata != NULL);
+
+	pricerdata -> maxvarsround = 
+		MAX(5, probdata -> nSets) * MAX(50, probdata -> nnodes) / 50;
+	pricerdata -> constraintssize = probdata -> constraintssize;
+	pricerdata -> constraints = probdata -> constraints;
+
+
+	SCIP_CALL( SCIPallocBlockMemoryArray(scip, &(pricerdata -> pi), probdata -> constraintssize) );
+
+	pricerdata -> bbnode = NULL;
+
+	return SCIP_OKAY;
+}
+
+static
+SCIP_DECL_PRICEREXITSOL(pricerExitSolEdgepartition)
+{
+	SCIP_PRICERDATA* pricerdata;
+	assert(scip != NULL);
+	assert(pricer != NULL);
+
+	pricerdata = SCIPpricerGetData(pricer);
+	assert(pricerdata != NULL);
+
+	SCIPfreeBlockMemoryArray(scip, &(pricerdata -> pi), pricerdata -> constraintssize);
+
+	return SCIP_OKAY;
+}
+
 
 static
 SCIP_DECL_PRICERREDCOST(pricerRedcostEdgePartition)
@@ -75,8 +128,30 @@ SCIP_DECL_PRICERREDCOST(pricerRedcostEdgePartition)
 
 	*result = SCIP_SUCCESS;
 
-
+	/* get the dual variable value */
+	for(int i = 0; i < pricerdata -> constraintssize - 1; ++i)
+	{
+		pricerdata -> pi[i] = SCIPgetDualsolSetppc(scip, pricerdata -> constraints[i]);
+	}
+	pricerdata -> pi[pricerdata -> constraintssize - 1] = SCIPgetDualsolKnapsack(scip, pricerdata -> constraints[pricerdata -> constraintssize -1]);
 	
+	pricerdata -> nsetsfound = 0;
+
+	//pricer heuristic
+	if(pricerdata -> usePriceHeur)
+	{
+		/* using pricer heurisitc */
+	}
+
+	/* 
+	* using exact method (integer programming) to prove the optimal lp-sol of current master problem 
+	* of find a new variable which can improve the current master problem
+	*/
+	if(pricerdata -> nsetsfound == 0)
+	{
+
+	}
+
 	return SCIP_OKAY;
 }
 
@@ -98,6 +173,9 @@ SCIP_RETCODE SCIPincludePricerEdgePartition(
 	/* include variable pricer */
 	SCIP_CALL( SCIPincludePricerBasic(scip, &pricer, PRICER_NAME, PRICER_DESC, PRICER_PRIORITY, PRICER_DELAY,
          pricerRedcostEdgePartition, NULL, pricerdata) );
+
+	SCIP_CALL( SCIPsetPricerInitsol(scip, pricer, pricerInitSolEdgepartition) );
+	SCIP_CALL( SCIPsetPricerExitsol(scip, pricer, pricerExitSolEdgepartition) );
 
 	return SCIP_OKAY;
 }
